c672804d6e2f82cef685cb7bd4a3b40813a697e5
TheWolek <pawelm916@wp.pl>
2021-06-05 18:08:07 +0200

    sales endpoints

--- data.py
+++ data.py
@@ -1,6 +1,13 @@
-CLOTHES = [
-    {"id":0,"name":"T-shirt","oldPrice":49.99,"newPrice":39.99},
-    {"id":1,"name":"Shirt","oldPrice":149.99,"newPrice":129.99},
-    {"id":2,"name":"Pants","oldPrice":39.99,"newPrice":29.99},
-    {"id":3,"name":"Jacket","oldPrice":59.99,"newPrice":59.99}
+CLOTHES_SALES_FEMALE = [
+    {"id": 0, "name": "T-shirt", "regularPrice": 49.99, "newPrice": 39.99},
+    {"id": 1, "name": "Shirt", "regularPrice": 149.99, "newPrice": 129.99},
+    {"id": 2, "name": "Skirt", "regularPrice": 39.99, "newPrice": 29.99},
+    {"id": 3, "name": "Jacket", "regularPrice": 59.99, "newPrice": 59.99},
+]
+
+CLOTHES_SALES_MALE = [
+    {"id": 0, "name": "T-shirt", "regularPrice": 49.99, "newPrice": 39.99},
+    {"id": 1, "name": "Jeans", "regularPrice": 69.99, "newPrice": 59.99},
+    {"id": 2, "name": "T-shirt", "regularPrice": 39.99, "newPrice": 29.99},
+    {"id": 3, "name": "Jacket", "regularPrice": 59.99, "newPrice": 59.99},
 ]
--- flaskapi.py
+++ flaskapi.py
@@ -1,13 +1,20 @@
 from flask import Flask, json
+from flask_cors import CORS
 
 import data
 
 api = Flask(__name__)
+CORS(api)
 
 
-@api.route("/", methods=["GET"])
-def get_items():
-    return json.dumps(data.CLOTHES)
+@api.route("/sales/female", methods=["GET"])
+def get_items_fe():
+    return json.dumps(data.CLOTHES_SALES_FEMALE)
+
+
+@api.route("/sales/male", methods=["GET"])
+def get_items_ma():
+    return json.dumps(data.CLOTHES_SALES_MALE)
 
 
 if __name__ == "__main__":
